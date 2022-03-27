8b0e203936045a1d57f97b9ada6a539e0f65e018
Wiechsaa <szymon.sudull.dev@gmail.com>
2022-03-27 22:36:46 +0200

    Done

--- app.py
+++ app.py
@@ -1,16 +1,71 @@
-from flask import Flask, render_template, request
+import string
+import random
+
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///urls.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class Url(db.Model):
+    id = db.Column("id", db.Integer, primary_key=True)
+    long = db.Column("long", db.String())
+    short = db.Column("short", db.String(3))
+
+    def __init__(self, long, short):
+        self.long = long
+        self.short = short
+
+
+def shorten_url():
+    letters = string.ascii_lowercase + string.ascii_uppercase
+    while True:
+        rand_letters = random.choices(letters, k=3)
+        rand_letters = ''.join(rand_letters)
+        short_url = Url.query.filter_by(short=rand_letters).first()
+        if not short_url:
+            return rand_letters
+
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 
 @app.route('/', methods=['GET', 'POST'])
 def root():
-    if request.method == 'GET':
-        return render_template("home.html")
-    elif request.method == 'POST':
+    if request.method == 'POST':
         url_received = request.form['url_input']
-        return url_received
-    
+        existing_url = Url.query.filter_by(long=url_received).first()
+        if existing_url:
+            # return f"{existing_url.short}"
+            return redirect(url_for("display_short_url", url=existing_url.short))
+        else:
+            short_url = shorten_url()
+            new_url = Url(long=url_received, short=short_url)
+            db.session.add(new_url)
+            db.session.commit()
+            return redirect(url_for("display_short_url", url=short_url))
+    elif request.method == 'GET':
+        return render_template("home.html")
+
+
+@app.route('/display/<url>')
+def display_short_url(url):
+    return render_template('shorturl.html', short_url_display=url)
+
+
+@app.route('/<short_url>')
+def redirection(short_url):
+    long_url = Url.query.filter_by(short=short_url).first()
+    if long_url:
+        return redirect(long_url.long)
+    else:
+        return f"<h1>URL doesn't exist</h1>"
 
 
 if __name__ == '__main__':
